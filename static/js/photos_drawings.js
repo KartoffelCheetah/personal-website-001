b1064cef265f4bb1e2e5b5b496528914a18ef962
KartoffelCheetah <32495404+KartoffelCheetah@users.noreply.github.com>
2018-02-05 16:07:31 +0100

    Disabled maximized view of images in touch screen devices. Reason: different zoom method. Moved some functions into a separate file.

---
+++ static/js/photos_drawings.js
@@ -0,0 +1,29 @@
+// FULLSCREEN
+$(function getFullscreen(){
+	// toggles between display or hide modal
+	// https://davidwalsh.name/fullscreen
+	var
+		modal100 = $('.non-touch #modal100'),
+		modal100van = document.getElementById('.non-touch modal100')
+	;
+	$('#TheImage').click(function(){
+		modal100.removeClass('hidden');
+		// modal100van['requestFullscreen'] = modal100van.requestFullscreen || modal100van.mozRequestFullScreen || modal100van.webkitRequestFullscreen || modal100van.msRequestFullscreen;
+		// if (modal100van.requestFullscreen) modal100van.requestFullscreen();
+	})
+	modal100.click(function(){
+		modal100.addClass('hidden');
+		// document['exitFullscreen'] = document.exitFullscreen || document.mozCancelFullScreen || document.webkitExitFullscreen || document.msExitFullscreen;
+		// if (document.exitFullscreen) document.exitFullscreen();
+	})
+})
+// OVERFLOW SLIDER
+$(function sliderOverflow(){
+	// adding .overflowed class to element
+	var /*global variable for toggleOverflow*/
+		photoSlider = $('.photo-slider')
+	;
+
+	$(toggleOverflow.bind(window, photoSlider)); //on ready
+	$(window).resize(toggleOverflow.bind(window, photoSlider)); //on resize
+})
