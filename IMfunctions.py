0bc76425cb452fd6f4286c3c07f24568aa7410fc
KartoffelCheetah <32495404+KartoffelCheetah@users.noreply.github.com>
2018-02-08 20:41:38 +0100

    Added strip to check output functions. Sometimes datetime original container linebreak.

--- IMfunctions.py
+++ IMfunctions.py
@@ -9,10 +9,10 @@ import os
 # supported ImageMagic Formats?
 def getDateTimeOriginal(filePath) :
     """If image has DateTimeOriginal then returns it as a string. If it's missing or there isn't EXIF at all then returns empty string."""
-    return subprocess.check_output(['identify', '-format', '%[EXIF:DateTimeOriginal]', filePath], timeout=3).decode('utf8')
+    return subprocess.check_output(['identify', '-format', '%[EXIF:DateTimeOriginal]', filePath], timeout=3).decode('utf8').strip()
 def getDimensions(filePath) :
     """Returns dimensions of image in a tuple."""
-    x,y = subprocess.check_output(['identify', '-format', '%[w]x%[h]', filePath], timeout=3).decode('utf8').split('x')
+    x,y = subprocess.check_output(['identify', '-format', '%[w]x%[h]', filePath], timeout=3).decode('utf8').strip().split('x')
     return int(x), int(y)
 def createThumbnail(originPath, destinationPath, size='200x200') :
     """Creates a thumbnail image out of originPath in destinationPath of size (which defaults to maxWidth:200, maxHeight:200) and keeps ratio. The function creates directory if not exists already. Returns the subprocess's response."""
