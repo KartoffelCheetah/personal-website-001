eaddfebbe7910fd40f03898d2051e20ab2cfbdda
KartoffelCheetah <kartoffel.cheetah@gmail.com>
2020-08-07 02:01:49 +0200

    Image res addition in controller

--- app/controllers/image_resource.py
+++ app/controllers/image_resource.py
@@ -1,5 +1,9 @@
 #pylint: disable=R0201
 """Image Resource Controller"""
+import os
+from PIL import Image
+from werkzeug.datastructures import FileStorage
+from werkzeug.utils import secure_filename
 from flask import current_app
 from flask_restx import Resource, abort, fields
 import flask_login
@@ -29,6 +33,16 @@ IMAGE_DOC = API.model('ImageResource', {
     ),
 })
 
+IMAGE_POST_DOC = API.parser()
+IMAGE_POST_DOC.add_argument(
+        'resource',
+        type=str,
+        help='Unique resource identifier.',
+        location='form',
+        required=True
+        )
+IMAGE_POST_DOC.add_argument('imagedata', location='files', type=FileStorage, required=True)
+
 @IMAGE_RES_NAMESPACE.route(ROUTING['RI']['IMAGE']['LIST'])
 class ImageResourceList(Resource):
     """Handles image resources"""
@@ -39,11 +53,26 @@ class ImageResourceList(Resource):
         return ImageResourceEntity.query.all()
 
     @flask_login.login_required
-    @API.doc(security='cookie', body=IMAGE_DOC)
+    @API.doc(security='cookie', body=IMAGE_POST_DOC)
+    @API.expect(IMAGE_POST_DOC, as_list=True)
     @API.marshal_with(IMAGE_DOC, as_list=True)
     def post(self):
         """Adds a new image resource."""
-        new_media = ImageResourceEntity(**API.payload, width=0, height=0)
+        is_conflict = False
+        args = IMAGE_POST_DOC.parse_args()
+        securefname = secure_filename(args['resource'])
+        securefpath = os.path.join(os.getenv('FOLDER_UPLOAD'), securefname)
+        if ImageResourceEntity.query.filter_by(resource=securefname).first():
+            current_app.logger.exception('Post image resource image already exists in db.')
+            is_conflict = True
+        if os.path.exists(securefpath):
+            current_app.logger.exception('Post image resource image already exists in fs.')
+            is_conflict = True
+        if is_conflict:
+            return abort(409)
+        args['imagedata'].save(securefpath)
+        width, height = Image.open(securefpath).size
+        new_media = ImageResourceEntity(resource=securefname, width=width, height=height)
         database = current_app.config['database']
         try:
             database.session.add(new_media)
