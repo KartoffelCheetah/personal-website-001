1b4b045d31b9de1c5cf7855880076a26aac1e101
KartoffelCheetah <kartoffel.cheetah@gmail.com>
2021-03-14 23:34:45 +0100

    Adds the header and footer, link,button style

--- client/Index.vue
+++ client/Index.vue
@@ -1,21 +1,24 @@
 <template lang="pug">
 header
-  h1.heading1 header
-  h2.heading2 header
-  h3.heading3 header
-  h4.heading4 header
-  h5.heading5 header
-  h6.heading6 header
-main
+  h1.heading1.indexTitle
+    span Kartoffel&#8203;Cheetah's
+    span.heading2 website
+main.indexMain
   router-view
-    p qwertzuiopőúasdfghjkléáűíyxcvbnmöüó
-    p QWERTZUIOPŐÚASDFGHJKLÉÁŰÍYXCVBNMÖÜÓ
-    br
-    p I'm a front-end developer.
-      | adgnafg dhguifgfigiof experience as a programmer.
-      | I enjoy programming in JavaScript and Python3, designing websites and doing 2D art.
-      | Whenever possible I use open-source software on Linux. 🐧
-footer footer
+  p qwertzuiopőúasdfghjkléáűíyxcvbnmöüó
+  p QWERTZUIOPŐÚASDFGHJKLÉÁŰÍYXCVBNMÖÜÓ
+  br
+  p I'm a front-end developer.
+    | adgnafg dhguifgfigiof experience as a programmer.
+    | I enjoy programming in JavaScript and Python3, designing websites and doing 2D art.
+    | Whenever possible I use open-source software on Linux. 🐧
+footer
+  p i want some icons to the links
+  ul.indexContact
+    li
+      a.link(href="https://github.com/KartoffelCheetah") GitHub
+    li
+      a.link(href="https://gitlab.com/KartoffelCheetah") GitLab
 </template>
 <script lang="ts">
   import { defineComponent } from 'vue';
@@ -24,3 +27,21 @@ footer footer
     name: 'Index',
   });
 </script>
+<style scoped>
+  .index
+    &Title
+      display flex
+      flex-direction column
+      padding-left 8px
+      border-bottom 1em solid var(--brown)
+      border-left .3em solid var(--wheat)
+      margin-bottom 5px
+      color black
+      text-shadow 1px 1px 2px var(--wheat)
+    &Contact
+      display flex
+      padding-top 2em
+      gap .8em
+    &Main
+      padding 0 3px
+</style>
--- client/pages/Home.vue
+++ client/pages/Home.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-div Home
+div Home!!!!!
 </template>
 
 <script lang="ts">
