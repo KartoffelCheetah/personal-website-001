5d77106e07402ac2b37e096a82ee7378a0eafc99
KartoffelCheetah <kartoffel.cheetah@gmail.com>
2023-01-08 17:21:35 +0100

    Adds profileBadge for myself. Small rephrase.

--- client/clientApp/pages/Home.vue
+++ client/clientApp/pages/Home.vue
@@ -1,13 +1,14 @@
 <template lang="pug">
 div
   p
+    profile-badge(:profile-badge="KartoffelCheetah", alt-override="", style="margin-right: 3px;")
     | Hey, I'm <b>KartoffelCheetah</b> :) !
-    | Front-end webdev working from Budapest, Hungary.
+    | Front-end webdev working in e-commerce.
+    | Base of operations in Budapest, Hungary since 2018.
     | I'm a staunch supporter of software accessibility
-    | and an advocate for Open Source solutions.
+    | and an advocate of Open Source.
   p
-    | Currently I'm working in the e-commerce field.
-    | Linux is my choice of OS both for work and for personal use.
+    | My choice of OS is Linux both for work and for personal use.
     | I also dabble a bit in 2D art.
   h2.heading3 Contact
   p TODO
@@ -17,10 +18,18 @@ div
 <script lang="ts">
   import { defineComponent } from 'vue';
   import ImageResource from '../components/ImageResource.vue';
+  import ProfileBadge, { KartoffelCheetah } from '../components/ProfileBadge.vue';
+
   export default defineComponent({
     name: 'Home',
     components: {
       ImageResource,
+      ProfileBadge,
+    },
+    data () {
+      return {
+        KartoffelCheetah,
+      };
     },
   });
 </script>
