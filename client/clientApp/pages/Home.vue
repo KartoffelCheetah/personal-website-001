c13acbeadb9e67ea8b19ec234880db4a00d7ae01
KartoffelCheetah <kartoffel.cheetah@gmail.com>
2023-02-28 20:57:44 +0100

    Changes homepage content, rewords introduction

--- client/clientApp/pages/Home.vue
+++ client/clientApp/pages/Home.vue
@@ -2,12 +2,17 @@
 div
 	p
 		profile-badge(:profile-badge="KCProfileBadge", alt-override="", style="margin-right: 3px;")
-		| Hey, I'm <b>KartoffelCheetah</b> :) !
-		| Front-end webdev working in e-commerce.
-		| Base of operations in Budapest, Hungary since 2018.
-		| I'm a staunch supporter of software accessibility
-		| and an advocate of Open Source.
+		| Hey, I'm <b>KartoffelCheetah</b>, web frontend developer :) !
+		br
+		| I'm occasionally working on a couple of smaller projects, but most
+		| of my time is spent on a supermarket chain's international
+		| <em>e-commerce</em> platform (and on the sites running on this
+		| platform).
+		br
+		| My base of operations is in Budapest, Hungary :) .
 	p
+		| It's worth noting that I'm a staunch supporter of software
+		| accessibility and an advocate of Open Source technologies.
 		| My choice of OS is Linux both for work and for personal use.
 		| I also dabble a bit in 2D art.
 	h2.heading3 Contact
